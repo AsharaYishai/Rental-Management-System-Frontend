54cd337fbbb60d349caa84641a92d884b2f70a38
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 15:05:52 +0530

    add customer ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './common/nav/nav.component';
+import { CustomerManagementComponent } from './page/customer-management/customer-management.component';
+import { ItemManagementComponent } from './page/item-management/item-management.component';
+import { RentalManagementComponent } from './page/rental-management/rental-management.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,CustomerManagementComponent,ItemManagementComponent,RentalManagementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/page/customer-management/customer-management.component.ts
+++ src/app/page/customer-management/customer-management.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../common/nav/nav.component';
 
 @Component({
   selector: 'app-customer-management',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './customer-management.component.html',
   styleUrl: './customer-management.component.css'
 })
 export class CustomerManagementComponent {
 
+  
+
+  addCustomer(){
+
+  }
 }
