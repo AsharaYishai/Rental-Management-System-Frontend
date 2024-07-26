d988be9a0938db8e68ee22b6f32978134570e09f
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 15:34:23 +0530

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CustomerManagementComponent } from './page/customer-management/customer-management.component';
+import { ViewAllCustomerComponent } from './page/view-all-customer/view-all-customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-customer",
+        component:CustomerManagementComponent
+    },
+    {
+        path:"view-all-customer",
+        component:ViewAllCustomerComponent
+    }
+];
