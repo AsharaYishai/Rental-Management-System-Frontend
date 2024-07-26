fa9baa6f8bdea3d61ce553711e242339be8a09b0
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 15:26:16 +0530

    add customer /customer  insert  database

--- src/app/page/customer-management/customer-management.component.ts
+++ src/app/page/customer-management/customer-management.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../common/nav/nav.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-management',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,HttpClientModule,FormsModule,CommonModule],
   templateUrl: './customer-management.component.html',
   styleUrl: './customer-management.component.css'
 })
 export class CustomerManagementComponent {
 
-  
+  public customerObj={
+    name:"",
+    city:"",
+    contact:""
+  }
 
-  addCustomer(){
+  constructor(private http:HttpClient){
 
   }
+
+  addCustomer(){
+    this.http.post("http://localhost:8080/customer-controller/add-customer",this.customerObj).subscribe(
+      (data=>{
+        console.log(data);
+        
+      }
+    )
+  )}
 }
