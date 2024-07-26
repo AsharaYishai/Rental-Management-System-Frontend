d9d90b593e1dd273a21923c425cf543dcb75df19
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 15:56:30 +0530

    view all customer table and install bootstrap icon

--- src/app/page/view-all-customer/view-all-customer.component.ts
+++ src/app/page/view-all-customer/view-all-customer.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../common/nav/nav.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-view-all-customer',
   standalone: true,
-  imports: [],
+  imports: [NavComponent,CommonModule,FormsModule,HttpClientModule],
   templateUrl: './view-all-customer.component.html',
   styleUrl: './view-all-customer.component.css'
 })
 export class ViewAllCustomerComponent {
+
+  public customerList:any;
+
+  constructor(private http:HttpClient){
+    this.loadcustomerTable();
+  }
+
+  loadcustomerTable(){
+    this.http.get("http://localhost:8080/customer-controller/get-all-customer").subscribe(res=>{
+      this.customerList=res;
+      console.log(res);
+      
+    })
+  }
 
 }
