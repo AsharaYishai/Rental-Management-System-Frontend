646435f36bed89324866acd37d3d7c6fbe8359e5
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 16:08:44 +0530

    sweetAlert

--- src/app/page/customer-management/customer-management.component.ts
+++ src/app/page/customer-management/customer-management.component.ts
@@ -3,6 +3,7 @@ import { NavComponent } from '../../common/nav/nav.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-customer-management',
@@ -26,6 +27,11 @@ export class CustomerManagementComponent {
   addCustomer(){
     this.http.post("http://localhost:8080/customer-controller/add-customer",this.customerObj).subscribe(
       (data=>{
+        Swal.fire({
+          title: "Customer Added!",
+          text: "You clicked the button!",
+          icon: "success"
+        });
         console.log(data);
         
       }
--- src/app/page/view-all-customer/view-all-customer.component.ts
+++ src/app/page/view-all-customer/view-all-customer.component.ts
@@ -3,6 +3,7 @@ import { NavComponent } from '../../common/nav/nav.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-customer',
@@ -25,6 +26,43 @@ export class ViewAllCustomerComponent {
       console.log(res);
       
     })
+  }
+
+  deleteCustomer(customer:any){
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+
   }
 
 }
